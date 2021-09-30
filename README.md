a7a779db8bc3dc8378c7b3c941499ecb9c228219
Akash Sahu <akasharunsahu@gmail.com>
2021-10-01 00:04:20 +0530

    Added feature to detect post language

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Instagram Graph API based BOT to post top/trending based posts on Instagram on y
 Step 1 : Create a Facebook and get the necessary Credential for the project.
          Ref : https://github.com/imakashsahu/Instagram-Graph-API-Python
 
+Step 2 : Enter the Credentials for Graph API in to the config file (instagram_config.ini) in etc/ directory. 
+
--- instagram.py
+++ instagram.py
@@ -4,6 +4,10 @@ import requests
 import json
 import re
 import datetime
+import uuid
+
+from requests.api import post
+import langdetect
 
 class InstagramBot():
 	def __init__(self):
@@ -24,34 +28,64 @@ class InstagramBot():
 	# Returns list of hashtags from config to monitor
 	def get_hashtags_list(self):
 		hashtag = re.split(', ', self.hashtags)
-		self.logger.info(f"Monitoring [{self.hashtags}] list of hashtags in this run")
+		self.logger.info(f"Monitoring '{self.hashtags}' list of hashtags in this run")
 		return hashtag
 	
 	def get_hashtag_id(self, hashtag):
-		graph_api_call = requests.get(f"https://graph.facebook.com/ig_hashtag_search?"
-							f"user_id={self.instagram_id}&q={hashtag}"
-							f"&access_token={self.access_token}")
-		hashtag_id = graph_api_call.json()['data'][0]['id']
-		return hashtag_id
+		try:
+			self.logger.info(f"Fetching hashtag id for #{hashtag}")
+			graph_api_call = requests.get(f"https://graph.facebook.com/ig_hashtag_search?"
+								f"user_id={self.instagram_id}&q={hashtag}"
+								f"&access_token={self.access_token}"
+							)
+			hashtag_id = graph_api_call.json()['data'][0]['id']
+			return hashtag_id
+		except Exception as error:
+			self.logger.error(f"[API HASHTAG ID] : {error}")
+
 
 	def get_hashtag_media(self, hashtag_id):
-		media_fields = "id,permalink,comments_count,like_count,media_type,media_url,timestamp,caption,children{id,permalink,media_type,media_url,timestamp}"
-		graph_api_call = requests.get(f"https://graph.facebook.com/{hashtag_id}/"
-							f"top_media?fields={media_fields}"
-							f"&user_id={self.instagram_id}"
-							f"&access_token={self.access_token}")
-		hashtag_posts = graph_api_call.json()['data']
-		return hashtag_posts
+		try:
+			# Specify if to fetch top_media  or recent_media
+			media_type = 'recent_media'
+			media_fields = """id,permalink,comments_count,like_count,media_type,media_url,
+					timestamp,caption,children{id,permalink,media_type,media_url,timestamp}"""
+			self.logger.info(f"Fetching posts for hashtag id '{hashtag_id}'")
+			graph_api_call = requests.get(f"https://graph.facebook.com/{hashtag_id}/"
+								f"{media_type}?fields={media_fields}"
+								f"&user_id={self.instagram_id}"
+								f"&access_token={self.access_token}"
+							)
+			hashtag_posts = graph_api_call.json()['data']
+			return hashtag_posts
+		except Exception as error:
+			self.logger.error(f"[API MEDIA FETCH] : {error}")
+
+	def detect_post_lang(self, hashtag_post):
+		try:
+			for i in range(0, 25):
+				if hashtag_post[i]['media_type'] in ('IMAGE', 'VIDEO'):
+					post_lang = langdetect.detect(hashtag_post[i]['caption'])
+					if post_lang == 'en':
+						self.logger.info(f"Selected '{hashtag_post[i]['permalink']}' to be posted")
+						return hashtag_post[i]
+		except Exception as error:
+			self.logger.error(f"[LANGUAGE DETECT] : {error}")
 
 	def start(self):
-		# data = requests.get(f"https://graph.facebook.com/ig_hashtag_search?user_id={self.instagram_id}&q=python&access_token={self.access_token}")
-		# print(data.json())
-		self.logger.info(f"Started a new run at : {datetime.datetime.now()}")
-		for hashtag in self.get_hashtags_list():
-			hashtag_id = self.get_hashtag_id(hashtag)
-			hashtag_post = self.get_hashtag_media(hashtag_id)
-			for i in range(0, 25):
-				print(hashtag_post[i])
+		try:
+			self.logger.info(f"Don't do anything I would do, "
+						f"and definitely don't do anything I wouldn't do…")
+			for hashtag in self.get_hashtags_list():
+				hashtag_id = self.get_hashtag_id(hashtag)
+				hashtag_post = self.get_hashtag_media(hashtag_id)
+				
+				# Check if Post language is English
+				post = self.detect_post_lang(hashtag_post)
+				if post:
+					print(post['caption'])
+		except Exception as error:
+			self.logger.error(f"[START METHOD] : {error}")
 
 if __name__ == "__main__":
 	InstagramBot().start()
